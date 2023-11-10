8b8734c8af4deec5c24f3e53e72f9fa42a583902
haha41 <thgml500@gmail.com>
2023-11-11 01:16:13 +0900

    feat: todolist 렌더링

--- 03.react/src/App.tsx
+++ 03.react/src/App.tsx
@@ -1,48 +1,53 @@
 // import Footer from "../../layout/Footer";
 // import Header from "../../layout/Header";
 
+import { useEffect, useState } from "react";
 import instance from "./api/instance";
 import styles from "./pages/TodoList/TodoList.module.css";
 
 function App() {
-  // const App = async function () {
-  // const response = await instance.get<TodoListResponse>("/");
-  // console.log(response.data);
+  const [data, setData] = useState<{ items: TodoItem[] | undefined }>();
 
-  // const handleSubmit = () => {
-  //   instance
-  //     .post("", {
-  //       title: title,
-  // content: content,
-  //     })
-  //     .then(() => {
-  //       window.location.href = "/";
-  //     })
-  //     .catch((error) => {
-  //       console.error(error);
-  //     });
-  // };
+  async function fetchData() {
+    try {
+      const response = await instance.get<TodoListResponse>("/");
+      console.log(response.data);
+      setData(response.data);
+
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
       <div id={styles.content}>
         <ul className={styles.todoList}>
-          <li className={styles.todoListItem}>
-            <div className={styles.itemWrapper}>
-              <input type="checkbox" />
-              <a className={styles.undoItemLink} href="">
-                javascript 공부
-              </a>
-            </div>
-            <div className={styles.todoActionWrapper}>
-              <a title="수정" className={styles.todoEditLink} href="">
-                <i className={styles.far}>수정</i>
-              </a>
-              <button title="삭제" className={styles.deleteBtn}>
-                <i className={styles.far}>삭제</i>
-              </button>
-            </div>
-          </li>
+          {data?.items?.map((item: TodoItem, i: number) => {
+            return (
+              <li key={`${item._id}-${i}`} className={styles.todoListItem}>
+                <div className={styles.itemWrapper}>
+                  <input type="checkbox" />
+                  <a className={styles.undoItemLink} href="">
+                    {item.title}
+                  </a>
+                </div>
+                <div className={styles.todoActionWrapper}>
+                  <a title="수정" className={styles.todoEditLink} href="">
+                    <i className={styles.far}>수정</i>
+                  </a>
+                  <button title="삭제" className={styles.deleteBtn}>
+                    <i className={styles.far}>삭제</i>
+                  </button>
+                </div>
+              </li>
+            );
+          })}
         </ul>
         <a href="" className={styles.registBtn} title="할일등록">
           <i className={styles.far}>등록</i>
