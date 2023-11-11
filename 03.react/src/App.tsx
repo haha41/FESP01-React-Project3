d1d17b3f72cbfc83bd90d0343a5223b0971dad20
haha41 <thgml500@gmail.com>
2023-11-11 19:36:47 +0900

    Feat: 할일 완료 체크 기능 구현

--- 03.react/src/App.tsx
+++ 03.react/src/App.tsx
@@ -24,24 +24,68 @@ function App() {
     fetchData();
   }, []);
 
+  const handleCheckTodo = function (id: number) {
+    setData((prevData) => {
+      const updatedItems = prevData?.items?.map((item) => {
+        if (item._id === id) {
+          return {
+            ...item,
+            done: !item.done,
+          };
+        }
+        return item;
+      });
+      return { ...prevData, items: updatedItems };
+    });
+  };
+
+  const handleTodoEdit = function (e) {
+    e.preventDefault();
+    console.log("수정");
+  };
+
+  const handleTodoDelete = function (e) {
+    e.preventDefault();
+    console.log("삭제");
+  };
+
   return (
     <>
       <div id={styles.content}>
         <ul className={styles.todoList}>
-          {data?.items?.map((item: TodoItem, i: number) => {
+          {data?.items?.map((item, i) => {
             return (
               <li key={`${item._id}-${i}`} className={styles.todoListItem}>
                 <div className={styles.itemWrapper}>
-                  <input type="checkbox" />
-                  <a className={styles.undoItemLink} href="">
+                  <input
+                    type="checkbox"
+                    onChange={() => handleCheckTodo(item._id)}
+                    checked={item.done}
+                  />
+                  <a
+                    className={
+                      item.done ? styles.doneItemLink : styles.undoItemLink
+                    }
+                    href=""
+                  >
                     {item.title}
                   </a>
                 </div>
                 <div className={styles.todoActionWrapper}>
-                  <a title="수정" className={styles.todoEditLink} href="">
-                    <i className={styles.far}>수정</i>
+                  <a
+                    title="수정"
+                    className={styles.todoEditLink}
+                    href=""
+                    onClick={handleTodoEdit}
+                  >
+                    {/* <i className={styles.far}>수정</i> */}
+                    <i className="far, fa-pen-to-square">수정</i>
                   </a>
-                  <button title="삭제" className={styles.deleteBtn}>
+                  <button
+                    title="삭제"
+                    className={styles.deleteBtn}
+                    onClick={handleTodoDelete}
+                  >
                     <i className={styles.far}>삭제</i>
                   </button>
                 </div>
