d8312a197269852e2e8284113f2109f7b5d11dc4
haha41 <thgml500@gmail.com>
2023-11-12 00:39:59 +0900

    Feat: NotFound 페이지 추가

--- 03.react/src/routes.tsx
+++ 03.react/src/routes.tsx
@@ -8,6 +8,7 @@ import { Regist } from "./pages/Regist/Regist";
 import { List } from "./pages/List/List";
 import { Update } from "./pages/Update/Update";
 import { TodoInfo } from "./pages/TodoInfo/TodoInfo";
+import NotFound from "./pages/error/NotFound";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +17,7 @@ export const router = createBrowserRouter(
       <Route path="regist" element={<Regist />} />
       <Route path="update/:todoId" element={<Update />} />
       <Route path="todoInfo/:todoId" element={<TodoInfo />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
